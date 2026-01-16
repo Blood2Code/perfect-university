443c778a8d6b6a189c5ce68be821bf7a8b8e0dc6
umarmuhendis <umarraxmanberdiyev@gmail.com>
2026-01-16 14:15:32 +0500

    practice 7

---
+++ src/cpp/practice_work_7/practice_1/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int age = 18;
+    double height = 1.70;
+    char letter = 'A';
+    bool isStudent = true;
+
+    cout << "Age: " << age << endl;
+    cout << "Height: " << height << endl;
+    cout << "First letter: " << letter << endl;
+    cout << "Is student: " << isStudent << endl;
+
+    return 0;
+}
---
+++ src/cpp/practice_work_7/practice_3/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    const double PI = 3.14;
+    cout << "PI = " << PI << endl;
+    return 0;
+}
